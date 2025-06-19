d3f7c6747b192bd541402584edb8d60b0d295989
hwangchahae <wndal12@gmail.com>
2025-06-19 16:06:58 +0900

    .

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,19 @@
+from typing import Any
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post  # Import the Post model
 # Create your views here.
 class PostListView(ListView):
     model = Post  # Specify the model to use
-    
     template_name = 'blog/post_list.html'  # Specify the template to use
     context_object_name = 'posts'  # Specify the context variable name
+        
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['user_id'] = self.request.session.get('user_id')
+        context['username'] = self.request.session.get('username')
+        return context
+
 
 class PostDetailView(DetailView):
     model = Post  # Specify the model to use
@@ -29,5 +36,5 @@ class PostDeleteView(DeleteView):
     model = Post  # Specify the model to use
     # 삭제를 시도할때 template_name을 지정 ,get방식으로 호출이 되어야만 해당 html이 렌더링됨
     # post로 요청하면 삭제가 진행됨
-    template_name = 'blog/post_confirm_delete.html'  # Specify the template to use
+    # template_name = 'blog/post_confirm_delete.html'  # Specify the template to use
     success_url = '/blog'  # Redirect to the post list after successful deletion
--- loginout/views.py
+++ loginout/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.views import View
+from . models import UserProfile
+from django.contrib import messages
+from django.contrib.auth.hashers import make_password, check_password
+# from django.contrib.auth import authenticate, login, logout
 # Create your views here.
+# CustomLoginView, CustomLogoutView
+
+class CustomLoginView(View):
+    template_name = 'loginout/login.html'
+    def get(self, request):
+        return render(request, self.template_name)
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        try:
+            user = UserProfile.objects.get(username=username)
+            if check_password(password, user.password):  # 비밀번호 확인
+                request.session['user_id'] = user.id
+                request.session['username'] = user.username
+                return redirect('/blog')
+            else:
+                messages.error(request, "아이디 또는 패스워드가 틀렸습니다.")
+                return render(request, self.template_name)
+        except Exception as e:
+            print(f"Error during login: {e}")
+            messages.error(request, str(e))
+            return render(request, self.template_name)
+
+class CustomLogoutView(View):
+    def post(self, request):
+        # 로그아웃 처리
+        request.session.flush()
+        return redirect('/blog')
+
+class SignupView(View):
+    template_name = 'loginout/signup.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        email = request.POST.get('email')        
+        # 중복체크
+        if UserProfile.objects.filter(username=username).exists():
+            messages.error(request, "이미 존재하는 아이디입니다.")
+            return render(request, self.template_name)
+        if UserProfile.objects.filter(email=email).exists():
+            messages.error(request, "이미 존재하는 이메일입니다.")
+            return render(request, self.template_name)
+        # 회원가입 처리
+        user = UserProfile.objects.create(username=username, 
+                                          password=make_password(password), 
+                                          email=email)
+        messages.success(request, "회원가입이 완료되었습니다.")
+        return redirect('login')
